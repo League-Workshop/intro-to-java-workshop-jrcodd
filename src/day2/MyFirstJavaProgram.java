061621f7874e516d2b97114f24ea6c87942f6b50
gabrielsong9674 <league@WorthyMini-5.attlocal.net>
2017-11-20 13:53:17 -0800

    day 2 workshop

--- src/day2/MyFirstJavaProgram.java
+++ src/day2/MyFirstJavaProgram.java
@@ -8,8 +8,28 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	System.out.println("Hello World");
+	Robot fred = new Robot("batman");
+	fred.setPenColor(55, 100, 100);
+		fred.penUp();
+		fred.setAngle(0);
+		fred.setSpeed(1000);
+		fred.turn(90);
+		fred.sparkle();
+		fred.penDown();
+		fred.move(200);
+		fred.turn(90);
+		fred.move(200);
+		fred.turn(90);
+		fred.setPenColor(255, 0, 0);
+		fred.move(200);
+		fred.turn(90);
+		fred.setPenColor(0, 255, 0);
+		fred.move(200);
+		fred.unSparkle();
+		fred.penUp();
+		fred.move(900);
 	
-		
-		
+	
 	}
 }
--- src/day2/Spiral.java
+++ src/day2/Spiral.java
@@ -6,20 +6,21 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+		Robot jeff = new Robot("batman");
 		// 5. Set your robot's pen to the down position
-		
+		jeff.penDown();
 		// 3. Set the robot to go at max speed (10)
-		
+		jeff.setSpeed(100000);
 		// 4. Do the following (steps 6-8) 75 times
-		
+		for(int i =0; i<100; i++) {
 			// 7. Change the pen color to random
-		
+		jeff.setRandomPenColor();
 			// 6. Move the robot 5 times the current line number you are drawing (5*i)
-			
+			jeff.move(i*5);
 			// 2. Turn the robot 1/7 of 360 degrees to the right
-		
+		jeff.turn(360/7);
 			// 8. Set the pen width to i
-			
+	    jeff.setPenWidth(i);
+	}
 	}
 }
